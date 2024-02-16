31761f80a4f044ec955b1521746a59e086f9d4b4
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2024-02-16 22:00:04 +0100

    Add hello world http api

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world\n"))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
