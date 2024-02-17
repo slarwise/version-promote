44d2569f36468c61eab714fa369d888fb298976b
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2024-02-17 02:49:05 +0100

    Add ping endpoint

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world\n"))
 	})
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong\n"))
+	})
 
 	http.ListenAndServe(":8080", nil)
 }
